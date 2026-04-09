f82afd7142283a0000434453a08b398a5edd81be
NESSTERN <ernestarnoux@gmail.com>
2026-04-09 08:59:20 +0200

    Init 2 codes

---
+++ Clément.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 08:58:25 2026
+
+@author: Clément
+"""
+
---
+++ Ernest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 08:58:26 2026
+
+@author: Ernest
+"""
+
